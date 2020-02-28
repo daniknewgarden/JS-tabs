e7fb5b8a9dae5e6e71611ebb9266e1fd2761f0df
Danik <daniknewgarden@gmail.com>
2020-02-28 12:07:49 +0200

    Aded dcripts file

---
+++ tabs.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.header-tab'),
+        info = document.querySelector('.header'),
+        tabContent = document.querySelectorAll('.tab-content');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+});
