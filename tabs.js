0271adc5a3f8a0528f0df810e51338d539b6b6a8
Danik <daniknewgarden@gmail.com>
2020-03-12 11:09:26 +0200

    Aded adaptive to demo and active class to button

--- tabs.js
+++ tabs.js
@@ -2,9 +2,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
 
-    let tab = document.querySelectorAll('.header-tab'),
-        info = document.querySelector('.header'),
-        tabContent = document.querySelectorAll('.tab-content');
+    let tab = document.querySelectorAll('.header-tab'), //class of your tab button
+        info = document.querySelector('.header'), //class of  your menu with tab buttons
+        tabContent = document.querySelectorAll('.tab-content'); //class of your tab with some content
 
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
@@ -24,15 +24,17 @@ window.addEventListener('DOMContentLoaded', function () {
 
     info.addEventListener('click', function (event) {
         let target = event.target;
-        if (target && target.classList.contains('header-tab')) {
+        if (target && target.classList.contains('header-tab')) { //class of your tab button
             for (let i = 0; i < tab.length; i++) {
+                tab[i].classList.remove('show');  //if you want to add some styles on active button
+                event.target.classList.add('show'); //if you want to add some styles on active button
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
-                    break;
                 }
             }
         }
     });
+
 
 });
